9f96dfd7944125d6001bd3725687220578baf985
Mauricio MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2024-04-06 10:27:04 -0300

    Exercise solution

--- src/main/java/HandshakeCalculator.java
+++ src/main/java/HandshakeCalculator.java
@@ -1,9 +1,18 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 class HandshakeCalculator {
 
     List<Signal> calculateHandshake(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+        var result = new ArrayList<Signal>();
+        var reversedBinary = new StringBuilder(Integer.toBinaryString(number)).reverse().toString().toCharArray();
 
+        for (int i = 0; i < Integer.min(4, reversedBinary.length); i++)
+            if (reversedBinary[i] == '1') result.add(Signal.values()[i]);
+
+        if (reversedBinary.length > 4 && reversedBinary[4] == '1') Collections.reverse(result);
+
+        return result;
+    }
 }
