2acd5f645430baf8510738f1ce0ab8cbe9cf9e3a
Mauricio MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2024-04-07 14:13:56 -0300

    Refactor

--- src/main/java/HandshakeCalculator.java
+++ src/main/java/HandshakeCalculator.java
@@ -1,18 +1,24 @@
-import java.util.ArrayList;
 import java.util.Collections;
+import java.util.EnumSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class HandshakeCalculator {
 
     List<Signal> calculateHandshake(int number) {
-        var result = new ArrayList<Signal>();
-        var reversedBinary = new StringBuilder(Integer.toBinaryString(number)).reverse().toString().toCharArray();
-
-        for (int i = 0; i < Integer.min(4, reversedBinary.length); i++)
-            if (reversedBinary[i] == '1') result.add(Signal.values()[i]);
-
-        if (reversedBinary.length > 4 && reversedBinary[4] == '1') Collections.reverse(result);
-
+        var result = EnumSet.allOf(Signal.class).stream()
+                .filter(s -> (signalToInt(s) & number) > 0)
+                .collect(Collectors.toList());
+        if ((number & 0b10000) > 0) Collections.reverse(result);
         return result;
+    }
+
+    private static int signalToInt(Signal signal) {
+        return switch (signal) {
+            case WINK -> 0b00001;
+            case DOUBLE_BLINK -> 0b00010;
+            case CLOSE_YOUR_EYES ->0b00100;
+            case JUMP ->0b01000;
+        };
     }
 }
